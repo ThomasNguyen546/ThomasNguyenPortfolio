ee647432d23928b2c199bc0c7006503948c810ea
Thomas Nguyen <thomasnguyen546@gmail.com>
2021-03-01 23:27:10 -0700

    Updates

---
+++ precache-manifest.02a2f724b20ac26bb8021200be898a4e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce5ee1aa83f4a628a8b49e100c16c8c",
+    "url": "/ThomasNguyenPortfolio/index.html"
+  },
+  {
+    "revision": "1dcf228ca275291ebc4a",
+    "url": "/ThomasNguyenPortfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "a9e59572f70549049c6e",
+    "url": "/ThomasNguyenPortfolio/static/css/main.a98e4c30.chunk.css"
+  },
+  {
+    "revision": "1dcf228ca275291ebc4a",
+    "url": "/ThomasNguyenPortfolio/static/js/2.1657d9e6.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/ThomasNguyenPortfolio/static/js/2.1657d9e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9e59572f70549049c6e",
+    "url": "/ThomasNguyenPortfolio/static/js/main.cc739111.chunk.js"
+  },
+  {
+    "revision": "b5eaf65dd0992d23b379",
+    "url": "/ThomasNguyenPortfolio/static/js/runtime-main.b9f5f7c3.js"
+  },
+  {
+    "revision": "d48d021d8c14945bcb0d73dc64eb333a",
+    "url": "/ThomasNguyenPortfolio/static/media/BudgetTracker.d48d021d.png"
+  },
+  {
+    "revision": "e01d18aed39a5ac2794fb242dfc7d6e7",
+    "url": "/ThomasNguyenPortfolio/static/media/FortressOfFandom.e01d18ae.png"
+  },
+  {
+    "revision": "4442e43e89e4a61240c346e483056706",
+    "url": "/ThomasNguyenPortfolio/static/media/HolidayPlanner.4442e43e.png"
+  },
+  {
+    "revision": "50c833eb330ea6919babcf126dd57f25",
+    "url": "/ThomasNguyenPortfolio/static/media/NoteTaker.50c833eb.png"
+  },
+  {
+    "revision": "fd2b18baee76b54d13fde059307fdc78",
+    "url": "/ThomasNguyenPortfolio/static/media/PizzaHunt.fd2b18ba.png"
+  },
+  {
+    "revision": "b3dda369b1f7dc9d160e594e35dd58ba",
+    "url": "/ThomasNguyenPortfolio/static/media/ProfilePicture.b3dda369.JPG"
+  },
+  {
+    "revision": "beda4faa90f92d43419a6db2bb171867",
+    "url": "/ThomasNguyenPortfolio/static/media/RecipeRoundup.beda4faa.png"
+  },
+  {
+    "revision": "eb0fbf987eb91c6ea68af9ebcd310d59",
+    "url": "/ThomasNguyenPortfolio/static/media/Resume.eb0fbf98.pdf"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/ThomasNguyenPortfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ThomasNguyenPortfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ThomasNguyenPortfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ThomasNguyenPortfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ThomasNguyenPortfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ThomasNguyenPortfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/ThomasNguyenPortfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/ThomasNguyenPortfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/ThomasNguyenPortfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/ThomasNguyenPortfolio/static/media/ionicons.621bd386.svg"
+  }
+]);
